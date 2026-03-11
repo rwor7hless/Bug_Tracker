8eda3d4fd5b528f22dae613945b35afb0df88d28
Roman <rwor7hless@gmail.com>
2026-03-11 12:02:14 +0300

    [FIX] UI

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,7 +2,7 @@
 set -e
 
 echo "Running prisma db push..."
-npx prisma db push --skip-generate
+npx prisma db push --skip-generate --accept-data-loss
 
 echo "Starting app..."
 exec node dist/src/index.js
